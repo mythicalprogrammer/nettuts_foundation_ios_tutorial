aeabc589e464145b3a5e5b3edd366dcc77c51038
Anthony Doan <anthonydoan@android_a100001784f692.gateway.2wire.net>
2013-07-07 19:01:24 -0700

    Initial Commit

---
+++ Foundation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Foundation
+//
+//  Created by Anthony Doan on 7/7/13.
+//  Copyright (c) 2013 Mobile Tuts. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
